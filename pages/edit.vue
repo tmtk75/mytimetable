0fa12e95b184e9f3d67ce712c32b0651e82fa269
Tomotaka Sakuma <ktmtmks@gmail.com>
2019-02-23 18:50:24 +0900

    fix: snackbar timeout

--- pages/edit.vue
+++ pages/edit.vue
@@ -22,7 +22,7 @@
       </v-card>
     </v-dialog>
 
-    <v-snackbar v-model="snackbar" :timeout="2000">
+    <v-snackbar v-model="snackbar" :timeout="1500">
       <v-layout justify-center>{{ snackbarText}}</v-layout>
     </v-snackbar>
   </v-container>
